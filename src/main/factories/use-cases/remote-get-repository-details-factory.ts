5be3b6f620ac9317172f904324804db3e0b97e9b
Sunderhus <matheus.sunderhus@gmail.com>
2023-05-17 20:05:50 -0300

    feat: add FetchHttpClientAdapter and add it to the GetRepositoryDetails

--- src/main/factories/use-cases/remote-get-repository-details-factory.ts
+++ src/main/factories/use-cases/remote-get-repository-details-factory.ts
@@ -1,11 +1,13 @@
 import { RemoteGetRepositoryDetails } from '../../../data/use-cases/RemoteGetRepositoryDetails'
 import { GetRepositoryDetails } from '../../../domain/use-cases/GetRepositoryDetails'
 import { makeApiUrl } from '../http/api-url-factory'
-import { makeAxiosHttpClientAdapter } from '../http/axios-http-client-adapter-factory'
+import { makeFetchHttpClientAdapter } from '../http/fetch-http-client-adapter-factory'
+// import { makeAxiosHttpClientAdapter } from '../http/axios-http-client-adapter-factory'
 
+// You can change to use Axios or Fetch, it is up to you.
 export const makeRemoteGetRepositoryDetails = (): GetRepositoryDetails => {
   return new RemoteGetRepositoryDetails(
     makeApiUrl('repos'),
-    makeAxiosHttpClientAdapter(),
+    makeFetchHttpClientAdapter(),
   )
 }
