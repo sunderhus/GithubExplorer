435244d53a32ab4ba9b113e371b70b4cf080923c
sunderhus <matheus.sunderhus@gmail.com>
2023-10-22 15:09:39 -0300

    fix: add generic type to CacheSaveRepositories use case to avoid bind cacheSave to a repository model

--- src/data/protocols/cache/SaveCacheClientProtocol.ts
+++ src/data/protocols/cache/SaveCacheClientProtocol.ts
@@ -1,5 +1,3 @@
-import { Repository } from '../../../domain/models/Repository'
-
 export interface SaveCacheProtocol {
-  save(key: string, content: Repository[]): void
+  save<T = unknown>(key: string, content: T): void
 }
--- src/infra/cache/LocalStorageSaveCacheClienteAdapter.ts
+++ src/infra/cache/LocalStorageSaveCacheClienteAdapter.ts
@@ -1,12 +1,11 @@
 import { SaveCacheProtocol } from '../../data/protocols/cache/SaveCacheClientProtocol'
-import { Repository } from '../../domain/models/Repository'
 
 export class LocalStorageSaveCacheClienteAdapter implements SaveCacheProtocol {
-  save(key: string, content: Repository[]): void {
+  save(key: string, content: unknown): void {
     localStorage.setItem(key, this.adapt(content))
   }
 
-  private adapt(content: Repository[]): string {
+  private adapt(content: unknown): string {
     return JSON.stringify(content)
   }
 }
